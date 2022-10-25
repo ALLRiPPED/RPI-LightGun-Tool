d400ad90f7f6c63d95179606cb009ed061f43af5
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-24 19:08:35 -0600

    Update RPI-Mouse-Gun-Menu.sh

--- RPI-Mouse-Gun-Menu.sh
+++ RPI-Mouse-Gun-Menu.sh
@@ -61,6 +61,38 @@ YOU WILL NEED TO DO THE FOLLOWING
 - LOAD NEW CONFIG 
 - GO TO PATH/COMING/SOON/"$1"-lightgun.cfg
 - SAVE CONFIG FOR "$2"" 0 0
+  local choice
+  while true; do
+    choice=$(dialog --backtitle "$BACKTITLE" --title "EXIT MESSAGE" \
+      --ok-label Install --cancel-label Exit \
+      --menu "DO YOU WANT TO MAKE A NEW SYSTEM FOR "$1" GUN GAMES ?" 40 60 40 \
+      1 "YES " \
+      2 "NO " \                
+      2>&1 >/dev/tty)
+
+    case "$choice" in
+    1) gun-system "arcade"  ;;
+    2) no  ;;
+    -) no ;;
+     *) break ;;
+    esac
+   done
 }
+
+function gun-system() {
+sudo cp "$1" config path -P "$1"
+mkdir "$HOME"/RetroPie/roms/gun-games/"$1"
+if [ ! -s "$HOME/.emulationstation/es_systems.cfg" ]; then sudo rm -f $HOME/.emulationstation/es_systems.cfg; fi
+if [ ! -f "$HOME/.emulationstation/es_systems.cfg" ]; then cp $HOME/.emulationstation/es_systems.cfg $HOME/RetroPie/retropiemenu/gamelist.xml; fi
+CONTENT1="\t<system>\n\t\t  <name>"$1"-guns</name>\n\t\t  <fullname>"$1" Gun Games</fullname> \n\t\t  <path>/home/pi/RetroPie/roms/gun-games/"$1"</path> \n\t\t  <extension>.zip .ZIP</extension> \n\t\t<command>/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ m "$1"-guns %ROM%</command> \n\t\t  <platform>"$1"-guns</platform> \n\t\t  <theme>"$1"</theme> \n\t\t</system>"
+C1=$(echo $CONTENT1 | sed 's/\//\\\//g')
+if grep -q "$1 "$HOME/.emulationstation/es_systems.cfg"; then echo "es_systems.cfg entry confirmed"
+else
+	sed "/<\/system>/ s/.*/${C1}\n&/" $HOME/.emulationstation/es_systems.cfg > $HOME/temp
+	cat $HOME/temp > $HOME/.emulationstation/es_systems.cfg
+	rm -f $HOME/temp
+fi
+}
+
 
 gun-menu
