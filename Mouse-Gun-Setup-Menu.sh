80bb5937f23804f53392c5cfc2f59f3f209b3ce9
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-11-04 20:24:55 -0600

    Update Mouse-Gun-Setup-Menu.sh

--- Mouse-Gun-Setup-Menu.sh
+++ Mouse-Gun-Setup-Menu.sh
@@ -25,19 +25,22 @@ function gun-menu() {
       3 "Atari2600 Setup" \
       4 "NES Setup" \
       5 "SNES Setup" \
+      6 "Mastersystem Setup" \
        + "<                                     >" \
        + "<---Mouse Lightgun Setup Menu Tools--->" \
        + "<                                     >" \
-      T1 "-UPDATE MOUSE LIGHTGUN SETUP MENU       " \
-      T2 "-REMOVE MOUSE LIGHTGUN SETUP MENU       " \                
+      T1 "-SETUP INFO/HELP         " \   
+      T2 "-UPDATE SETUP MENU       " \
+      T3 "-UNINSTALL SETUP MENU    " \                
       2>&1 >/dev/tty)
 
     case "$choice" in
     1) ra-gun "arcade"  ;;
     2) ra-gun "atari800"  ;;
     3) ra-gun "atari2600" ;;
-    4) ra-gun "nes" "FCEEUM" ;;
+    4) ra-gun "nes" "fceeum" ;;
     5) ra-gun "snes"   ;;
+    6) ra-gun "mastersystem" ;;
     T1) update-script ;;
     T2) remove-script ;;
     -) no ;;
@@ -48,30 +51,19 @@ function gun-menu() {
 
 function ra-gun() {
 if [ -f "RETROARCH CONFIG PATH" ]; then sudo rm RETROARCH CONFIG PATH; fi
-wget  https://github.com/Retro-Devils/RPI-Mouse-Gun-Setup/new/main/ra-configs/"$1"-lightgun.cfg -PPATH COMIGN SOON
-dialog  --sleep 1 --title ""$1" GUN SETUP INFO" --msgbox "
-----------ATTENTION!!!!!!----------
------------------------------------
-YOU WILL NEED TO DO THE FOLLOWING
-- OPEN GAME YOU WISH TO USE GUN WITH
-- OPEN RETROARCH SETTINGS
-- MAKE SURE GLOBAL CONFIGS ARE OFF
-- TURN ON PER CORE OVERRIDES
-- GO TO CONFIGURATION 
-- LOAD NEW CONFIG 
-- GO TO PATH/COMING/SOON/"$1"-lightgun.cfg
-- SAVE CONFIG FOR "$2"" 0 0
   local choice
   while true; do
     choice=$(dialog --backtitle "$BACKTITLE" --title "EXIT MESSAGE" \
       --ok-label Install --cancel-label Exit \
-      --menu "DO YOU WANT TO MAKE A NEW SYSTEM FOR "$1" GUN GAMES ?" 40 60 40 \
-      1 "YES " \
-      2 "NO " \                
+      --menu "WHAT OPTIONS DO YOU WANT FOR  "$1"?" 40 60 40 \
+      1 "APPLY CONFIG" \
+      2 "APPLY CONFIG & EDIT ES SYSTEMS " \ 
+      3 "DOWNLOAD & APPLY RETROARCH CONFIG" \
+      4 " " \
       2>&1 >/dev/tty)
 
     case "$choice" in
-    1) gun-system "arcade"  ;;
+    1) es-edit-gun "$1"  ;;
     2) no  ;;
     -) no ;;
      *) break ;;
@@ -79,20 +71,63 @@ YOU WILL NEED TO DO THE FOLLOWING
    done
 }
 
-function gun-system() {
-sudo cp "$1" config path -P "$1"
+function sa-gun() {
+if [ -f "RETROARCH CONFIG PATH" ]; then sudo rm RETROARCH CONFIG PATH; fi
+  local choice
+  while true; do
+    choice=$(dialog --backtitle "$BACKTITLE" --title "EXIT MESSAGE" \
+      --ok-label Install --cancel-label Exit \
+      --menu "WHAT OPTIONS DO YOU WANT FOR  "$1"?" 40 60 40 \
+      1 "MAKE GUN DIRECTORY FOR "$1"       " \
+      2 "MAKE DIRECTORY & EDIT ES SYSTEMS  " \ 
+      3 "DOWNLOAD & APPLY RETROARCH CONFIG " \
+      4 "APPLY ALL " \
+      2>&1 >/dev/tty)
+
+    case "$choice" in
+    1) make-directory "$1"  ;;
+    2) es-edit-gun "$1"  ;;
+    3) retroarch-config  ;;
+    4) apply-all-gun  ;;
+    -) no ;;
+     *) break ;;
+    esac
+   done
+}
+
+function make-directory() {
+mkdir "$HOME"/RetroPie/roms/gun-games/"$1"
+dialog  --sleep 1 --title "MAKE DIRECTORY EXIT MESSAGE" --msgbox "
+- A FOLDER HAS BEEN MADE UNDER Home/Pi/RetroPie/roms/gun-games/"$1" " 0 0
+}
+
+
+function es-edit-gun() {
+sudo cp "$1" config-----path -P "$1"
 mkdir "$HOME"/RetroPie/roms/gun-games/"$1"
 if [ ! -s "$HOME/.emulationstation/es_systems.cfg" ]; then sudo rm -f $HOME/.emulationstation/es_systems.cfg; fi
 if [ ! -f "$HOME/.emulationstation/es_systems.cfg" ]; then cp $HOME/.emulationstation/es_systems.cfg $HOME/RetroPie/retropiemenu/gamelist.xml; fi
 CONTENT1="\t<system>\n\t\t  <name>"$1"-guns</name>\n\t\t  <fullname>"$1" Gun Games</fullname> \n\t\t  <path>/home/pi/RetroPie/roms/gun-games/"$1"</path> \n\t\t  <extension>.zip .ZIP</extension> \n\t\t<command>/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ m "$1"-guns %ROM%</command> \n\t\t  <platform>"$1"-guns</platform> \n\t\t  <theme>"$1"</theme> \n\t\t</system>"
 C1=$(echo $CONTENT1 | sed 's/\//\\\//g')
-if grep -q "$1 "$HOME/.emulationstation/es_systems.cfg"; then echo "es_systems.cfg entry confirmed"
+if grep -q "$1" "$HOME/.emulationstation/es_systems.cfg"; then echo "es_systems.cfg entry confirmed"
 else
 	sed "/<\/system>/ s/.*/${C1}\n&/" $HOME/.emulationstation/es_systems.cfg > $HOME/temp
 	cat $HOME/temp > $HOME/.emulationstation/es_systems.cfg
 	rm -f $HOME/temp
 fi
+dialog  --sleep 1 --title "MAKE DIRECTORY & EDIT ES EXIT MESSAGE" --msgbox "
+- A FOLDER HAS BEEN MADE UNDER Home/Pi/RetroPie/roms/gun-games/"$1" 
+- home/Pi/.emulationstation/es_systems.cfg has been edited
+- Your configs for "$1" have been copied to guns config" 0 0
 }
 
+function retroarch-config() {
+sudo wget retroarch---config--path -P coming---soon
+}
+
+function apply-all-gun() {
+es-edit-gun 
+retroarch-config
+}
 
 gun-menu
