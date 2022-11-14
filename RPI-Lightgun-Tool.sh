2cc2ed51d52e501733b8baf9f21cd296fb3d4a72
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-11-13 17:14:48 -0700

    Update RPI-Lightgun-Tool.sh

--- RPI-Lightgun-Tool.sh
+++ RPI-Lightgun-Tool.sh
@@ -254,8 +254,8 @@ if [ ! -d "/opt/retropie/configs/$1" ]; then emu-error; fi
 sudo wget https://raw.githubusercontent.com/Retro-Devils/RPI-LightGun-Tool/main/ra-configs/wii-mote/"$1"-gun/retroarch-gun.cfg -P /opt/retropie/configs/"$1"/
 sudo chmod 755 /opt/retropie/configs/"$1"/retroarch-gun.cfg
 if [ ! -d "$HOME/RetroPie/roms/$1/gun-games/" ]; then mkdir "$HOME/RetroPie/roms/$1/gun-games/"; fi
-sudo cp /opt/retropie/configs/"$1"/emulators.cfg /opt/retropie/configs/"$1"/emulators-cfg.backup
-sudo wget https://raw.githubusercontent.com/Retro-Devils/RPI-LightGun-Tool/main/ra-configs/wii-mote/"$1"-gun/emulators.cfg -P /opt/retropie/configs/"$1"/
+#sudo cp /opt/retropie/configs/"$1"/emulators.cfg /opt/retropie/configs/"$1"/emulators-cfg.backup
+#sudo wget https://raw.githubusercontent.com/Retro-Devils/RPI-LightGun-Tool/main/ra-configs/wii-mote/"$1"/emulators.cfg -P /opt/retropie/configs/"$1"/
 if [ ! -f "/opt/retropie/configs/"$1"/emulators.cfg.bak" ] ; then
 sudo sed -i  .bak '"$1-gun = "/opt/retropie/emulators/retroarch/bin/retroarch -L /opt/retropie/libretrocores/$2/$3.so --config /opt/retropie/configs/$1/retroarch-gun.cfg %ROM%"' /opt/retropie/configs/"$1"/emulators.cfg
 dialog  --sleep 1 --title "GUN CONFIG COMPLETE" --msgbox "
