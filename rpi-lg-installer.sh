2b776618146dad4e58b21308c0e9d10c3f4ac866
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-11-14 20:30:45 -0700

    Update rpi-lg-installer.sh

--- rpi-lg-installer.sh
+++ rpi-lg-installer.sh
@@ -37,8 +37,57 @@ sudo chmod 755 /usr/local/bin/RPI-Lightgun-Tool
 }
 
 function info() {
-echo "blah blah blah "
-sleep 5
+dialog  --sleep 1 --title "---RPI LIGHTGUN TOOL INFO 1---" --msgbox "
+Info
+     This is a tool to make using Lightguns on Retropie as easy as possible.
+     Version:                BETA 1.01
+     Last Update:            11/5/22
+FEATURES
+      Grabs a pre setup retroarch-gun.cfg file for Retroarch systems.
+      Grabs or edits files on standalone emu configs.
+      Make new folders for gun games. 
+      Ask what type of gun your using, & loads different menus for different guns.
+      Makes a new emulator in retropie, called lightgun-systemname. ex lightgun-nes
+HOW TO USE?
+   In Retropie
+      1.Go to Retropie/Config
+      2.Launch RPI Lightgun Tool       
+      3.Select Gun Type
+      4.Select system/console
+      5.Press A/Enter
+      6.Select From The Options 
+   From Command Line/Terminal
+      1.Type RPI-Lightgun-Tool                  
+      2.See steps 3-6 above" 0 0
+      
+dialog  --sleep 1 --title "---RPI LIGHTGUN TOOL INFO 2---" --msgbox "
+FREQUENTLY ASKED QUESTIONS
+Where does it make new folder for gun games ?
+home/pi/RetroPie/roms/systemname/gun-games/
+
+What is new configs called? Where are they at?
+The new retroarch config is located at 
+            /opt/retropie/configs/systemname/retroarch-gun.cfg  
+            
+The new edited emulators.cfg is located at
+            /opt/retropie/configs/systemname/emulators.cfg
+            
+Your emulators.cfg backup is located at 
+            /opt/retropie/configs/systemname/emulators-cfg.backup
+            
+What gun(s) is this for ?
+- Aimtrack(untested, Unlikely)
+- AE Lightgun
+- Dolphin Bar with Wii mote
+- Gun4IR Lightgun
+- Infared Mouse Input Guns
+- Sinden Lightgun
+
+How do I undo the changes the script made?
+   - Open RPI Lightgun Tool
+   - Click Gun Type   
+   - Click on ---UNDO MENU---   
+   - Select system youd like to undo" 0 0
 }
 
 
