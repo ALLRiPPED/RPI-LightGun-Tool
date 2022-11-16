179c187e2a414bdbf886b3e87959ea0b4d75bd91
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-11-15 20:40:33 -0700

    Update rpi-lg-installer.sh

--- rpi-lg-installer.sh
+++ rpi-lg-installer.sh
@@ -28,11 +28,10 @@ function gun-install-menu() {
 function install() {
 if [ -f "$HOME/RetroPie/retropiemenu/RPI-Lightgun-Tool.sh" ]; then sudo rm $HOME/RetroPie/retropiemenu/RPI-Lightgun-Tool.sh; fi
 if [ -f "/usr/local/bin/RPI-Lightgun-Tool" ]; then sudo rm /usr/local/bin/RPI-Lightgun-Tool; fi
-if [ ! -d "/$HOME/Lightgun-Tool/" ]; then mkdir /$HOME/Lightgun-Tool/; fi
+if [ ! -d "/$HOME/Lightgun-Tool/" ]; then mkdir $HOME/Lightgun-Tool/; fi
 wget https://raw.githubusercontent.com/Retro-Devils/RPI-LightGun-Tool/main/RPI-Lightgun-Tool.sh -P $HOME/RetroPie/retropiemenu/
-wget        WII LAYOUT PICS 
-wget        SETUP VIDEO 
-wget        IR MOUSE AND KEYBOARD PICs
+wget https://raw.githubusercontent.com/Retro-Devils/RPI-LightGun-Tool/main/Lightgun-Tool/ir-mouse-layout.sh -P $HOME/Lightgun-Tool/
+wget https://raw.githubusercontent.com/Retro-Devils/RPI-LightGun-Tool/main/Lightgun-Tool/wii-layout.sh -P $HOME/Lightgun-Tool/ 
 sleep 1
 sudo cp $HOME/RetroPie/retropiemenu/RPI-Lightgun-Tool.sh -f /usr/local/bin/RPI-Lightgun-Tool
 sleep 1
